613afb8d690f483408c5f67be0e8eb386a3cbac2
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-01-26 21:10:23 -0800

    Push

--- wp-content/themes/optimalnutritionals/page-main.php
+++ wp-content/themes/optimalnutritionals/page-main.php
@@ -48,7 +48,7 @@ get_header(); ?>
 		
 		<div class="product_title_wrapper">
 			
-			<span class="product_title">Ubiquinol</span><!-- product_title -->
+			<h1 class="product_title">Ubiquinol</h1><!-- product_title -->
 			<span class="large_description">Softgels</span><!-- large_description -->
 			<span class="sub_description">CoQ10 100 mg 60</span><!-- sub_description -->
 			<span class="sub_description">antioxidant</span><!-- sub_description -->
@@ -73,6 +73,124 @@ get_header(); ?>
 
 
 <img class="product_image" src="<?php bloginfo('template_directory');?>/images/bottle.png"/><!-- product_image -->
+
+<section class="why">
+	
+	<h2>Why Ubiquinol?</h2>
+	
+	<span class="yellowline"></span><!-- yellowline -->
+	
+	<div class="why_content">
+	
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+		<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae</p>
+		
+		<a href="">
+			<img class="buy_now" src="<?php bloginfo('template_directory');?>/images/buynow.png"/>
+		</a>
+
+	</div><!-- why_content -->
+	
+</section><!-- why -->
+
+<section class="benefits">
+	
+	<h2>The Benefits <span>of Using Ubiquinol</span></h2>
+	
+	<span class="description">Lorem Ipsum cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun</span><!-- description -->
+	
+	<div class="benefits_wrapper">
+		
+		<div class="single_benefit">
+			
+			<div class="benefit_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/b1.png"/>
+			
+			</div><!-- benefit_img -->
+			
+			
+			<div class="benefit_content">
+				
+				<strong>Lorem Ipsum</strong> cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun
+				
+			</div><!-- benefit_content -->
+		
+		</div><!-- single_benefit -->
+		
+		<div class="single_benefit">
+			
+			<div class="benefit_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/b2.png"/>
+			
+			</div><!-- benefit_img -->
+			
+			
+			<div class="benefit_content">
+				
+				<strong>Lorem Ipsum</strong> cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun
+				
+			</div><!-- benefit_content -->
+		
+		</div><!-- single_benefit -->
+		
+		<div class="single_benefit">
+			
+			<div class="benefit_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/b3.png"/>
+			
+			</div><!-- benefit_img -->
+			
+			
+			<div class="benefit_content">
+				
+				<strong>Lorem Ipsum</strong> cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun
+				
+			</div><!-- benefit_content -->
+		
+		</div><!-- single_benefit -->
+		
+		<div class="single_benefit">
+			
+			<div class="benefit_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/b4.png"/>
+			
+			</div><!-- benefit_img -->
+			
+			
+			<div class="benefit_content">
+				
+				<strong>Lorem Ipsum</strong> cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun
+				
+			</div><!-- benefit_content -->
+		
+		</div><!-- single_benefit -->
+		
+		<div class="single_benefit">
+			
+			<div class="benefit_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/b5.png"/>
+			
+			</div><!-- benefit_img -->
+			
+			
+			<div class="benefit_content">
+				
+				<strong>Lorem Ipsum</strong> cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun
+				
+			</div><!-- benefit_content -->
+		
+		</div><!-- single_benefit -->
+		
+				
+	</div><!-- benfits_wrapper -->
+	
+</section><!-- benefits -->
 
 
 
