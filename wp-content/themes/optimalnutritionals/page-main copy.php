7bb1e3306d0d4464991d107d7b089724a0c68123
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-01-31 21:30:42 -0800

    Push

---
+++ wp-content/themes/optimalnutritionals/page-main copy.php
@@ -0,0 +1,389 @@
+<?php
+/**
+* Template Name: Main
+
+ */
+
+get_header(); ?>
+
+
+<?php $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>
+
+
+<section class="top_wrapper">
+	
+	
+	<div class="container">
+	
+	
+	<div class="inner_top">
+		
+		
+		
+		<span class="optimal_title">Optimal Nutritionals</span><!-- optimal_title -->
+		
+		<div class="product_title_wrapper">
+			
+			
+
+   
+   
+   <h1 class="product_title">Coenzyme</h1><!-- product_title -->
+
+
+
+
+			<span class="large_description">Softgels</span><!-- large_description -->
+			<span class="sub_description">CoQ10 100 mg 60</span><!-- sub_description -->
+			<span class="sub_description">antioxidant</span><!-- sub_description -->
+			
+		</div><!-- product_title_wrapper -->
+		
+		<div class="top_image_wrapper">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/woman.png"/>
+			
+		</div><!-- top_image_wrapper -->
+		
+		<div style="clear:both"></div>
+		
+	</div><!-- inner_top -->
+	
+<a href="">
+<img class="product_image" src="<?php bloginfo('template_directory');?>/images/bottle.png"/>
+</a>
+
+
+<div id="myform" class="form_wrapper">
+	
+	<span class="subheader">LEARN MORE ToDAY!</span>
+	<a class="tel" href="tel:">555.555.5555</a>
+	
+	<?php gravity_form(1, false, false, false, '', true, 12); ?>
+	
+
+	
+</div><!-- form_wrapper -->
+<div style="clear:both"></div>
+
+</div><!-- container -->
+	
+	
+	
+	<div class="purple_bar"></div><!-- purple_bar -->
+	
+	
+</section><!-- top_wrapper-->
+
+
+
+
+
+<section class="why">
+	
+	<h2>Why Ubiquinol?</h2>
+	
+	<span class="yellowline"></span><!-- yellowline -->
+	
+	<div class="why_content">
+	
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+		<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae</p>
+		
+		<a href="">
+			<img class="buy_now" src="<?php bloginfo('template_directory');?>/images/buynow.png"/>
+		</a>
+
+	</div><!-- why_content -->
+	
+</section><!-- why -->
+
+<section class="benefits">
+	
+	<h2>The Benefits <span>of Using&nbsp;Ubiquinol</span></h2>
+	
+	<span class="description">Lorem Ipsum cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun</span><!-- description -->
+	
+	
+	<div class="benefits_container">
+	
+	<div class="girl_wrapper">
+	
+		<img src="<?php bloginfo('template_directory');?>/images/girl.png"/>
+	
+	</div><!-- girl_wrapper -->
+	
+	<div class="benefits_wrapper">
+		
+		<div class="single_benefit">
+			
+			<div class="benefit_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/b1.png"/>
+			
+			</div><!-- benefit_img -->
+			
+			
+			<div class="benefit_content">
+				
+				<strong>Lorem Ipsum</strong> cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun
+				
+			</div><!-- benefit_content -->
+		
+		</div><!-- single_benefit -->
+		
+		<div class="single_benefit">
+			
+			<div class="benefit_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/b2.png"/>
+			
+			</div><!-- benefit_img -->
+			
+			
+			<div class="benefit_content">
+				
+				<strong>Lorem Ipsum</strong> cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun
+				
+			</div><!-- benefit_content -->
+		
+		</div><!-- single_benefit -->
+		
+		<div class="single_benefit">
+			
+			<div class="benefit_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/b3.png"/>
+			
+			</div><!-- benefit_img -->
+			
+			
+			<div class="benefit_content">
+				
+				<strong>Lorem Ipsum</strong> cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun
+				
+			</div><!-- benefit_content -->
+		
+		</div><!-- single_benefit -->
+		
+		<div class="single_benefit">
+			
+			<div class="benefit_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/b4.png"/>
+			
+			</div><!-- benefit_img -->
+			
+			
+			<div class="benefit_content">
+				
+				<strong>Lorem Ipsum</strong> cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun
+				
+			</div><!-- benefit_content -->
+		
+		</div><!-- single_benefit -->
+		
+		<div class="single_benefit">
+			
+			<div class="benefit_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/b5.png"/>
+			
+			</div><!-- benefit_img -->
+			
+			
+			<div class="benefit_content">
+				
+				<strong>Lorem Ipsum</strong> cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserun
+				
+			</div><!-- benefit_content -->
+		
+		</div><!-- single_benefit -->
+		
+				
+	</div><!-- benfits_wrapper -->
+	
+	</div><!-- benefits_container -->
+	
+</section><!-- benefits -->
+
+<section class="testimonials">
+	
+	<h2>Testimonials</h2>
+	
+	<span class="yellowline"></span><!-- yellowline -->
+	
+	
+	<div class="testimonials_wrapper">
+		
+		
+		<div class="single_testimonial">
+			
+			<div class="testimonial_profile">
+			
+				<img class="real_people" src="<?php bloginfo('template_directory');?>/images/real_people.png"/>
+			
+				<img class="testimonial_pic" src="<?php bloginfo('template_directory');?>/images/square.png"/>
+				
+				<span class="name">First Name</span><!-- name -->
+			
+			</div><!-- testimonial_profile -->
+			
+			<div class="testimonial_content">
+				
+				<p>“Lorem ipsum dolor sit amet, ipsum consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et enim magna aliqua. Ut enim ad tempor enim minim veniam”</p>
+				
+				
+			</div><!-- testimonial_content -->
+			
+		</div><!-- single_testimonial -->
+		
+		
+		<div class="single_testimonial">
+			
+			<div class="testimonial_profile">
+			
+				<img class="real_results" src="<?php bloginfo('template_directory');?>/images/real-results.png"/>
+			
+				<img class="testimonial_pic" src="<?php bloginfo('template_directory');?>/images/square.png"/>
+				
+				<span class="name">First Name</span><!-- name -->
+			
+			</div><!-- testimonial_profile -->
+			
+			<div class="testimonial_content">
+				
+				<p>“Lorem ipsum dolor sit amet, ipsum consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et enim magna aliqua. Ut enim ad tempor enim minim veniam”</p>
+				
+				
+			</div><!-- testimonial_content -->
+			
+		</div><!-- single_testimonial -->
+		
+		
+	</div><!-- testimonials_wrapper -->
+	
+</section><!-- testimonials -->
+
+<section class="buy_now_section">
+	
+	
+	<div class="buy_now_container">
+	
+	<div class="content">
+		
+		<span class="sub_header">Lorem ipsum dolor sit ame ipsum </span>
+		<span class="large_header">Lorem ipsum dolor sit ame</span>
+		
+		<a href="">
+			<img class="buy_now" src="<?php bloginfo('template_directory');?>/images/buynow.png"/>
+		</a>
+		
+	</div><!-- content -->
+
+	<a href="">
+	
+	<img class="buy_now_product_image" src="<?php bloginfo('template_directory');?>/images/bottle.png"/><!-- product_image -->
+	
+	</a>
+	
+	</div><!-- buy_now_container -->
+
+</section><!-- buy_now_section -->
+
+
+<section  class="other_products">
+	
+	<h2>See Our Other Products</h2>
+	<span class="yellowline"></span><!-- yellowline -->
+	
+	<div class="product_wrapper">
+		
+		<div class="single_product">
+			
+			<a href="">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/bottle.png"/>
+				<span class="product_title">Lorem Ipsum Dolor</span>
+				<span class="product_subtitle">Lorem Ipsum Dolor</span>
+				
+				<span class="buy_now_button">Buy Now</span><!-- buy_now_button -->
+				
+			</a>
+			
+		</div><!-- single_product -->
+		
+		<div class="single_product">
+			
+			<a href="">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/bottle.png"/>
+				<span class="product_title">Lorem Ipsum Dolor</span>
+				<span class="product_subtitle">Lorem Ipsum Dolor</span>
+				
+				<span class="buy_now_button">Buy Now</span><!-- buy_now_button -->
+				
+			</a>
+			
+		</div><!-- single_product -->
+		
+		<div class="single_product">
+			
+			<a href="">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/bottle.png"/>
+				<span class="product_title">Lorem Ipsum Dolor</span>
+				<span class="product_subtitle">Lorem Ipsum Dolor</span>
+				
+				<span class="buy_now_button">Buy Now</span><!-- buy_now_button -->
+				
+			</a>
+			
+		</div><!-- single_product -->
+		
+		<div class="single_product">
+			
+			<a href="">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/bottle.png"/>
+				<span class="product_title">Lorem Ipsum Dolor</span>
+				<span class="product_subtitle">Lorem Ipsum Dolor</span>
+				
+				<span class="buy_now_button">Buy Now</span><!-- buy_now_button -->
+				
+			</a>
+			
+		</div><!-- single_product -->
+		
+	</div><!-- product_wrapper -->
+	
+	
+	
+</section><!-- other_products -->
+
+
+<footer>
+	<span>Disclaimer: The information and recommendations provided on this website have not been evaluated by the Food and Drug Administration and are for educational purposes only. The products offered on this site are not intended to diagnose, treat, cure or prevent any disease. You should always ask your doctor before using any products.</span>
+</footer>
+
+<div class="mobile_bar">
+	
+	<a class="contact_us" href="#myform">Contact Us</a>
+	<a class="footer_buy_now" href="">Buy Now</a>
+	
+</div><!-- mobile_bar -->
+
+
+
+
+
+
+
+
+
+
+
+	
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/optimalnutritionals/products/testimonials.php
@@ -0,0 +1,57 @@
+<section class="testimonials">
+	
+	<h2>Testimonials</h2>
+	
+	<span class="yellowline"></span><!-- yellowline -->
+	
+
+	<div class="testimonials_wrapper">
+		
+		
+		<div class="single_testimonial">
+			
+			<div class="testimonial_profile">
+			
+				<img class="real_people" src="<?php bloginfo('template_directory');?>/images/real_people.png"/>
+			
+				<img class="testimonial_pic" src="<?php bloginfo('template_directory');?>/images/square.png"/>
+				
+				<span class="name">First Name</span><!-- name -->
+			
+			</div><!-- testimonial_profile -->
+			
+			<div class="testimonial_content">
+				
+				<p>“Lorem ipsum dolor sit amet, ipsum consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et enim magna aliqua. Ut enim ad tempor enim minim veniam”</p>
+				
+				
+			</div><!-- testimonial_content -->
+			
+		</div><!-- single_testimonial -->
+		
+		
+		<div class="single_testimonial">
+			
+			<div class="testimonial_profile">
+			
+				<img class="real_results" src="<?php bloginfo('template_directory');?>/images/real-results.png"/>
+			
+				<img class="testimonial_pic" src="<?php bloginfo('template_directory');?>/images/square.png"/>
+				
+				<span class="name">First Name</span><!-- name -->
+			
+			</div><!-- testimonial_profile -->
+			
+			<div class="testimonial_content">
+				
+				<p>“Lorem ipsum dolor sit amet, ipsum consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et enim magna aliqua. Ut enim ad tempor enim minim veniam”</p>
+				
+				
+			</div><!-- testimonial_content -->
+			
+		</div><!-- single_testimonial -->
+		
+		
+	</div><!-- testimonials_wrapper -->
+	
+</section><!-- testimonials -->
